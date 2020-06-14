93716fa5a63503cf194ff31840b9510baa0a6dc6
qinyuanmao <yuanmao.qin@longbridge.sg>
2020-06-15 03:20:04 +0800

    添加 jobs 销毁

--- jobs/ticker.go
+++ jobs/ticker.go
@@ -5,68 +5,96 @@ import (
 	"time"
 
 	"github.com/qinyuanmao/gomao/logger"
+	"github.com/thoas/go-funk"
 )
 
 type jobStatus int
 
-var (
-	jobChan  chan *tickerJob
-	stopChan chan string
+const (
+	running jobStatus = iota
+	stopped
 )
 
-const (
-	jobRunning jobStatus = iota
-	jobStopped
+var (
+	jobChan           chan *tickerJob
+	stopChan          chan string
+	finishChan        chan string
+	finishAllChan     chan struct{}
+	finishAllChanDown chan struct{}
 )
 
 type tickerJob struct {
 	jobName    string
 	jobFunc    func()
-	status     jobStatus
 	ticker     *time.Ticker
+	status     jobStatus
 	delayCount int
 }
 
 func init() {
-	var jobsMap = make(map[string]*tickerJob)
+	var jobsMap = make(map[string](*tickerJob))
 	jobChan = make(chan *tickerJob)
 	stopChan = make(chan string)
+	finishChan = make(chan string)
+	finishAllChan = make(chan struct{})
+	finishAllChanDown = make(chan struct{})
 	go func() {
-		for {
-			select {
-			case job := <-jobChan:
-				if _, has := jobsMap[job.jobName]; has {
-					jobsMap[job.jobName].status = jobStopped
-					delete(jobsMap, job.jobName)
-				}
-				jobsMap[job.jobName] = job
-				go startJob(jobsMap[job.jobName])
-			case jobName := <-stopChan:
-				jobsMap[jobName].status = jobStopped
+		for job := range jobChan {
+			if _, has := jobsMap[job.jobName]; has {
+				StopJob(job.jobName)
+			}
+			jobsMap[job.jobName] = job
+			go jobsMap[job.jobName].startJob()
+		}
+	}()
+	go func() {
+		for jobName := range stopChan {
+			if _, has := jobsMap[jobName]; has {
+				jobsMap[jobName].status = stopped
 				delete(jobsMap, jobName)
+			} else {
+				finishChan <- jobName
 			}
 		}
 	}()
+	go func() {
+		<-finishAllChan
+		var jobNames = funk.Map(jobsMap, func(jobName string, job *tickerJob) string {
+			return jobName
+		}).([]string)
+		funk.ForEach(jobNames, func(jobName string) {
+			StopJob(jobName)
+		})
+		finishAllChanDown <- struct{}{}
+	}()
 }
 
-func startJob(job *tickerJob) {
-	defer job.ticker.Stop()
-	defer logger.Info(fmt.Sprintf("%s job is stopped.", job.jobName))
+func (job *tickerJob) startJob() {
+	if job == nil {
+		return
+	}
+	defer func() {
+		job.ticker.Stop()
+		logger.Info(fmt.Sprintf("%s job is stopped.", job.jobName))
+		if job.delayCount == 0 { // delayCount = 0 表示是手动结束的任务
+			finishChan <- job.jobName
+		}
+	}()
 	logger.Info(fmt.Sprintf("%s job is started.", job.jobName))
 	var count = 0
 	for {
-		if job.status == jobRunning {
+		if job.status == running {
 			<-job.ticker.C
 			job.jobFunc()
+			count++
+			if job.delayCount != 0 && count >= job.delayCount {
+				if count >= job.delayCount {
+					stopChan <- job.jobName
+					return
+				}
+			}
 		} else {
 			return
-		}
-		count++
-		if job.delayCount != 0 && count >= job.delayCount {
-			if count >= job.delayCount {
-				stopChan <- job.jobName
-				return
-			}
 		}
 	}
 }
@@ -75,12 +103,26 @@ func StartJob(jobName string, delaytime time.Duration, delayCount int, fun func(
 	jobChan <- &tickerJob{
 		jobName:    jobName,
 		jobFunc:    fun,
-		status:     jobRunning,
 		ticker:     time.NewTicker(delaytime),
 		delayCount: delayCount,
+		status:     running,
 	}
 }
 
-func StopJob(key string) {
-	stopChan <- key
+func StopJob(jobName string) {
+	logger.Info(fmt.Sprintf("%s job is stopping.", jobName))
+	stopChan <- jobName
+	for jobName == <-finishChan {
+		return
+	}
+}
+
+func Destory() {
+	finishAllChan <- struct{}{}
+	<-finishAllChanDown
+	close(jobChan)
+	close(stopChan)
+	close(finishChan)
+	close(finishAllChan)
+	close(finishAllChanDown)
 }
--- test/jobs/test.go
+++ test/jobs/test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/qinyuanmao/gomao/logger"
 
 	"github.com/qinyuanmao/gomao/jobs"
+	_ "github.com/qinyuanmao/gomao/jobs"
 )
 
 func main() {
@@ -15,15 +16,33 @@ func main() {
 	jobs.StartJob("test1", 1*time.Second, 0, func() {
 		fmt.Println("test1 is running")
 	})
-	time.Sleep(10 * time.Second)
-	jobs.StopJob("test1")
+
+	time.Sleep(5 * time.Second)
+	// 重复 job 执行，先关闭原来的，再执行新的
+	jobs.StartJob("test1", 1*time.Second, 0, func() {
+		fmt.Println("test1—new is running")
+	})
 
 	// 定时任务，只执行 5 次，非永远任务不需要关闭，执行 5 次之后会自动停止
 	jobs.StartJob("test2", 1*time.Second, 5, func() {
 		fmt.Println("test2 is running")
 	})
 
+	// 定时任务，永远执行
+	jobs.StartJob("test3", 1*time.Second, 0, func() {
+		fmt.Println("test3 is running")
+	})
+
+	// 定时任务，永远执行
+	jobs.StartJob("test4", 2*time.Second, 0, func() {
+		fmt.Println("test4 is running")
+	})
+
 	listener.StopApplication(func() {
+		jobs.StopJob("test1")
+		jobs.StopJob("test2")
+		jobs.StopJob("test5") // 结束不存在的 job
+		jobs.Destory()
 		logger.Info("stop jobs")
 	})
 }
