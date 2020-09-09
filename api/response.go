011f0826e3dbc0abff68249b412594791d1fe077
qinyuanmao <yuanmao.qin@longbridge.sg>
2020-09-10 03:05:25 +0800

    封装 ApiHandler 返回

---
+++ api/response.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func SuccessResponse(response interface{}) (httpCode, resultCode int, message string, result interface{}) {
+	return http.StatusOK, SUCCESS, "Success", response
+}
+
+func WithParamNotFound(paramName string) (httpCode, resultCode int, message string, result interface{}) {
+	return http.StatusBadRequest, PARAMS_NOT_FOUNT, fmt.Sprintf("%s parameter not found!", paramName), nil
+}
+
+func WithParamError(paramName, errorMessage string) (httpCode, resultCode int, message string, result interface{}) {
+	return http.StatusBadRequest, PARAMS_ERROR, fmt.Sprintf("%s parameter not error: %s!", paramName, errorMessage), nil
+}
+
+func WithLogout() (httpCode, resultCode int, message string, result interface{}) {
+	return http.StatusUnauthorized, NOLOGIN, fmt.Sprintf("You are logout."), nil
+}
