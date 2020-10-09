a52bdf94beaf9f75fbef6c171421cedb461fb65b
qinyuanmao <yuanmao.qin@longbridge.sg>
2020-10-09 22:06:59 +0800

    添加文件接口操作封装

--- api/api.go
+++ api/api.go
@@ -11,28 +11,12 @@ import (
 )
 
 type ApiHandler func(ctx *gin.Context) (httpCode, resultCode int, message string, result interface{})
+type FileHandler func(ctx *gin.Context) (httpCode int, message string, bytes []byte, fileName, contentType string)
 
 func JsonApi(handler ApiHandler) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		httpCode, resultCode, message, result := handler(ctx)
-		if httpCode != http.StatusOK {
-			logger.Error(message)
-			webhook := viper.GetString("dingding_webhook")
-			env := viper.GetString("env")
-			if webhook != "" {
-				dingtalk.GetInstance().Notify(&dingtalk.DingTalkMsg{
-					MsgType: "markdown",
-					Markdown: dingtalk.Markdown{
-						Title: "监控报警",
-						Text:  fmt.Sprintf("## 【%s】[%s] 接口请求异常: %d\n\n > 错误信息: %s", env, ctx.Request.URL, httpCode, message),
-					},
-					At: dingtalk.At{
-						AtMobiles: []string{"18583872978"},
-						IsAtAll:   false,
-					},
-				})
-			}
-		}
+		sendDingTalk(ctx.Request.URL.String(), message, httpCode)
 		response := map[string]interface{}{
 			"code":    resultCode,
 			"message": message,
@@ -41,5 +25,44 @@ func JsonApi(handler ApiHandler) gin.HandlerFunc {
 			response["result"] = result
 		}
 		ctx.JSON(httpCode, response)
+	}
+}
+
+func FileApi(hander FileHandler) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		httpCode, message, bytes, fileName, contentType := hander(ctx)
+		sendDingTalk(ctx.Request.URL.String(), message, httpCode)
+
+		if httpCode != http.StatusOK {
+			ctx.Writer.WriteHeader(httpCode)
+			ctx.Writer.Write([]byte(message))
+			return
+		}
+
+		ctx.Writer.Header().Set("Content-Type", contentType)
+		ctx.Writer.Header().Set("Content-Disposition", fmt.Sprintf("attachment;filename=%s", fileName))
+		ctx.Writer.Header().Set("Accept-Length", fmt.Sprintf("%d", len(bytes)))
+		ctx.Writer.Write(bytes)
+	}
+}
+
+func sendDingTalk(url, message string, httpCode int) {
+	if httpCode != http.StatusOK {
+		logger.Error(message)
+		webhook := viper.GetString("dingding_webhook")
+		env := viper.GetString("env")
+		if webhook != "" {
+			dingtalk.GetInstance().Notify(&dingtalk.DingTalkMsg{
+				MsgType: "markdown",
+				Markdown: dingtalk.Markdown{
+					Title: "监控报警",
+					Text:  fmt.Sprintf("## 【%s】[%s] 接口请求异常: %d\n\n > 错误信息: %s", env, url, httpCode, message),
+				},
+				At: dingtalk.At{
+					AtMobiles: []string{"18583872978"},
+					IsAtAll:   false,
+				},
+			})
+		}
 	}
 }
