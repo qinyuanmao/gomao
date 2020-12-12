51a946168a17444d159538bcbb5ed2c536415fdb
秦源懋 <yuanmao.qin@longbridge.sg>
2020-12-12 17:10:36 +0800

    结果返回更新

--- api/response.go
+++ api/response.go
@@ -2,34 +2,33 @@ package api
 
 import (
 	"fmt"
-	"net/http"
 )
 
-func SuccessResponse(response ...interface{}) (httpCode, resultCode int, message string, result interface{}) {
-	return http.StatusOK, SUCCESS, "Success", response
+func SuccessResponse(response ...interface{}) (code ResultCode, message string, result interface{}) {
+	return SUCCESS, "Success", response
 }
 
-func WithParamNotFound(paramName string) (httpCode, resultCode int, message string, result interface{}) {
-	return http.StatusBadRequest, PARAMS_NOT_FOUNT, fmt.Sprintf("%s parameter is required!", paramName), nil
+func WithParamNotFound(paramName string) (code ResultCode, message string, result interface{}) {
+	return PARAMS_NOT_FOUNT, fmt.Sprintf("%s parameter is required!", paramName), nil
 }
 
-func WithParamError(paramName, errorMessage string) (httpCode, resultCode int, message string, result interface{}) {
-	return http.StatusBadRequest, PARAMS_ERROR, fmt.Sprintf("%s parameter not error: %s!", paramName, errorMessage), nil
+func WithParamError(paramName, errorMessage string) (code ResultCode, message string, result interface{}) {
+	return PARAMS_ERROR, fmt.Sprintf("%s parameter not error: %s!", paramName, errorMessage), nil
 }
 
-func WithLogout() (httpCode, resultCode int, message string, result interface{}) {
-	return http.StatusUnauthorized, NOLOGIN, fmt.Sprintf("You are logout."), nil
+func WithLogout() (code ResultCode, message string, result interface{}) {
+	return NOLOGIN, fmt.Sprintf("You are logout."), nil
 }
 
-func WithServerError(err error) (httpCode, resultCode int, message string, result interface{}) {
-	return http.StatusInternalServerError, SERVER_ERROR, err.Error(), nil
+func WithServerError(err error) (code ResultCode, message string, result interface{}) {
+	return SERVER_ERROR, err.Error(), nil
 }
 
-func WithRecordNotFound() (httpCode, resultCode int, message string, result interface{}) {
-	return http.StatusNotFound, RECORD_NOT_FOUND, "Record not found.", nil
+func WithRecordNotFound() (code ResultCode, message string, result interface{}) {
+	return RECORD_NOT_FOUND, "Record not found.", nil
 }
 
-func WithResponseError(err error, response ...interface{}) (httpCode, resultCode int, message string, result interface{}) {
+func WithResponseError(err error, response ...interface{}) (code ResultCode, message string, result interface{}) {
 	if err != nil {
 		return WithServerError(err)
 	}
