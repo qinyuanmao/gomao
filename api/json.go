c215673b2bb03f25a5032cd00daacb7f05301f70
秦源懋 <yuanmao.qin@longbridge.sg>
2020-12-21 06:24:29 +0800

    fix json body read

--- api/json.go
+++ api/json.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"bytes"
 	"io/ioutil"
 
 	"github.com/thoas/go-funk"
@@ -8,88 +9,89 @@ import (
 )
 
 func (ctx *Context) GetJsonInt(key string) (int64, error) {
-	request := ctx.Copy().Request
-	body, err := ioutil.ReadAll(request.Body)
+	data, err := ctx.GetRawData()
 	if err != nil {
 		return 0, err
 	}
-	value := gjson.Get(string(body), key)
+	ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+	value := gjson.Get(string(data), key)
 	return value.Int(), err
 }
 
 func (ctx *Context) GetJsonBool(key string) (bool, error) {
-	request := ctx.Copy().Request
-	body, err := ioutil.ReadAll(request.Body)
+	data, err := ctx.GetRawData()
 	if err != nil {
-		return false, err
+		return "", err
 	}
-	value := gjson.Get(string(body), key)
+	ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+	value := gjson.Get(string(data), key)
 	return value.Bool(), err
 }
 
 func (ctx *Context) GetJsonFloat(key string) (float64, error) {
-	request := ctx.Copy().Request
-	body, err := ioutil.ReadAll(request.Body)
+	data, err := ctx.GetRawData()
 	if err != nil {
 		return 0, err
 	}
-	value := gjson.Get(string(body), key)
+	ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+	value := gjson.Get(string(data), key)
 	return value.Float(), err
 }
 
 func (ctx *Context) GetJsonString(key string) (string, error) {
-	request := ctx.Copy().Request
-	body, err := ioutil.ReadAll(request.Body)
+	data, err := ctx.GetRawData()
 	if err != nil {
 		return "", err
 	}
-	value := gjson.Get(string(body), key)
+	ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+	value := gjson.Get(string(data), key)
 	return value.String(), err
 }
 
 func (ctx *Context) GetJsonStringArray(key string) ([]string, error) {
-	request := ctx.Copy().Request
-	body, err := ioutil.ReadAll(request.Body)
+	data, err := ctx.GetRawData()
 	if err != nil {
 		return []string{}, err
 	}
-	value := gjson.Get(string(body), key)
+	ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+	value := gjson.Get(string(data), key)
 	return funk.Map(value.Array(), func(value gjson.Result) string {
 		return value.String()
 	}).([]string), nil
 }
 
 func (ctx *Context) GetJsonIntArray(key string) ([]int64, error) {
-	request := ctx.Copy().Request
-	body, err := ioutil.ReadAll(request.Body)
+	data, err := ctx.GetRawData()
 	if err != nil {
 		return []int64{}, err
 	}
-	value := gjson.Get(string(body), key)
+	ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+	value := gjson.Get(string(data), key)
 	return funk.Map(value.Array(), func(value gjson.Result) int64 {
 		return value.Int()
 	}).([]int64), nil
 }
 
 func (ctx *Context) GetJsonFloatArray(key string) ([]float64, error) {
-	request := ctx.Copy().Request
-	body, err := ioutil.ReadAll(request.Body)
+	data, err := ctx.GetRawData()
 	if err != nil {
 		return []float64{}, err
 	}
-	value := gjson.Get(string(body), key)
+	ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+	value := gjson.Get(string(data), key)
 	return funk.Map(value.Array(), func(value gjson.Result) float64 {
 		return value.Float()
 	}).([]float64), nil
 }
 
 func (ctx *Context) GetJsonBoolArray(key string) ([]bool, error) {
-	request := ctx.Copy().Request
-	body, err := ioutil.ReadAll(request.Body)
+	data, err := ctx.GetRawData()
 	if err != nil {
 		return []bool{}, err
 	}
-	value := gjson.Get(string(body), key)
+	ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+
+	value := gjson.Get(string(data), key)
 	return funk.Map(value.Array(), func(value gjson.Result) bool {
 		return value.Bool()
 	}).([]bool), nil
